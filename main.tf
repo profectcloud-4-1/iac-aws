119f495f630044b83e5b321bb344a288513121a0
babogyeong <jbk021010@gmail.com>
2025-11-10 14:58:06 +0900

    ✨ [feature] #10 - root - main.tf output부분 삭제

--- main.tf
+++ main.tf
@@ -90,37 +90,6 @@ module "rds_payment" {
   security_group_ids    = [module.security.rds_payment_sg_id]
 }
 
-output "common_network" {
-  value = module.common_network.vpc_id
-}
-
-output "common_network_public_subnet_id" {
-  value = module.common_network.public_subnet_id
-}
-
-output "common_network_private_subnet_app_id" {
-  value = module.common_network.private_subnet_app_id
-}
-
-output "common_network_private_subnet_db_id" {
-  value = module.common_network.private_subnet_db_id
-}
-
-output "rds_user_endpoint" {
-  value = module.rds_user.endpoint
-}
-
-output "rds_product_endpoint" {
-  value = module.rds_product.endpoint
-}
-
-output "rds_order_endpoint" {
-  value = module.rds_order.endpoint
-}
-
-output "rds_payment_endpoint" {
-  value = module.rds_payment.endpoint
-}
 
 module "vpc_endpoint" {
   source = "./modules/vpc-endpoint"
