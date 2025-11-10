cdcd745c316ce4d815b56141ba52bd9f8e27bc5c
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-11-10 13:33:46 +0900

    fix: 한 정책에 여러 ecr 저장소 포함

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -6,11 +6,3 @@ output "repository_arn" {
   value = aws_ecr_repository.this.arn
 }
 
-output "pull_policy_arn" {
-  value = aws_iam_policy.pull.arn
-}
-
-output "push_policy_arn" {
-  value = aws_iam_policy.push.arn
-}
-
