b2cc7cadb631fb3e548bd864a4dab501842e3c8d
babogyeong <jbk021010@gmail.com>
2025-11-10 17:06:07 +0900

    ✨ [feature] #26 - presigned-s3-bucket, iam(presigned용, 주석처리)

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,19 @@
+output "bucket_id" {
+  description = "ID of the S3 bucket"
+  value       = aws_s3_bucket.this.id
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.this.arn
+}
+
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.this.bucket
+}
+/*
+output "iam_user_name" {
+  value = aws_iam_user.presigned_user.name
+}
+ */
---
+++ modules/s3/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning" {
+  description = "Enable versioning"
+  type        = bool
+  default     = false
+}
+
