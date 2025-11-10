afb9a824fb6a1342f12a41002a8ca23adfe77ac0
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-11-10 10:20:07 +0900

    refactor: subnet cidr

--- main.tf
+++ main.tf
@@ -26,3 +26,15 @@ module "common_network" {
 output "common_network" {
   value = module.common_network.vpc_id
 }
+
+output "common_network_public_subnet_id" {
+  value = module.common_network.public_subnet_id
+}
+
+output "common_network_private_subnet_app_id" {
+  value = module.common_network.private_subnet_app_id
+}
+
+output "common_network_private_subnet_db_id" {
+  value = module.common_network.private_subnet_db_id
+}
--- modules/common-network/main.tf
+++ modules/common-network/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "goorm" {
 
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.goorm.id
-  cidr_block = "10.9.0.0/20"
+  cidr_block = cidrsubnet(aws_vpc.goorm.cidr_block, 4, 0)
   tags = {
     Name = "goorm-public-subnet"
   }
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "private-app" {
   vpc_id = aws_vpc.goorm.id
-  cidr_block = "10.9.16.0/20"
+  cidr_block = cidrsubnet(aws_vpc.goorm.cidr_block, 4, 1)
   tags = {
     Name = "goorm-private-subnet-app"
   }
@@ -23,7 +23,7 @@ resource "aws_subnet" "private-app" {
 
 resource "aws_subnet" "private-db" {
   vpc_id = aws_vpc.goorm.id
-  cidr_block = "10.9.32.0/20"
+  cidr_block = cidrsubnet(aws_vpc.goorm.cidr_block, 4, 2)
   tags = {
     Name = "goorm-private-subnet-db"
   }
