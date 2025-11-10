675c3f6e354851b898a18aacca18a8666de3c162
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-11-10 15:33:32 +0900

    feature: nlb,vpclink sg

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -96,3 +96,51 @@ resource "aws_security_group" "rds_payment" {
   }
 }
 
+
+### SG - NLB, VPC Link ###
+resource "aws_security_group" "vpc_link" {
+  name        = "${var.name_prefix}-vpc-link-sg"
+  description = "VPC Link SG"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-vpc-link-sg"
+  }
+}
+
+locals {
+  ingress_ports = [80, 180, 81, 181, 82, 182, 83, 183]
+}
+resource "aws_security_group" "nlb" {
+  name        = "${var.name_prefix}-nlb-sg"
+  description = "NLB SG"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = local.ingress_ports
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      security_groups = [aws_security_group.vpc_link.id]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name_prefix}-nlb-sg"
+  }
+}
