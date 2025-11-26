654d29166ebf12985010e90c6d9948cc45e7e3d2
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-11-26 21:18:42 +0900

    fix: variables

--- variables.tf
+++ variables.tf
@@ -15,40 +15,6 @@ variable "aws_region" {
   default     = "ap-northeast-2"
 }
 
-variable "user_image" {
-  type        = string
-  description = "User service image"
-  default     = "please-fill/imagename:tag"
-}
-
-variable "product_image" {
-  type        = string
-  description = "Product service image"
-  default     = "please-fill/imagename:tag"
-}
-
-variable "order_image" {
-  type        = string
-  description = "Order service image"
-  default     = "please-fill/imagename:tag"
-}
-
-variable "payment_image" {
-  type        = string
-  description = "Payment service image"
-  default     = "please-fill/imagename:tag"
-}
-
-variable "cloudfront_domain_name" {
-  type        = string
-  description = "CloudFront domain name (예시: api.goorm.store)"
-
-  validation {
-    condition     = length(var.cloudfront_domain_name) > 0
-    error_message = "CloudFront domain name is required"
-  }
-}
-
 variable "presigned_bucket_name" {
   type        = string
   description = "Presigned bucket name (예시: goorm-presigned-bucket)"
