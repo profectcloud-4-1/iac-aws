83797f64f2d06bc85e79d500b5c45da438f2ccfd
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-11-27 12:41:49 +0900

    fix: disable addon

--- main.tf
+++ main.tf
@@ -150,40 +150,40 @@ module "eks" {
 
 
 # EKS 연결용 데이터 소스 (Helm/Kubernetes 프로바이더)
-data "aws_eks_cluster" "eks" {
-  depends_on = [module.eks]
-  name       = module.eks.cluster_name
-}
-data "aws_eks_cluster_auth" "eks" {
-  depends_on = [module.eks]
-  name       = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "eks" {
+#   depends_on = [module.eks]
+#   name       = module.eks.cluster_name
+# }
+# data "aws_eks_cluster_auth" "eks" {
+#   depends_on = [module.eks]
+#   name       = module.eks.cluster_name
+# }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   alias                  = "eks"
+#   host                   = data.aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
-provider "helm" {
-  alias = "eks"
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   alias = "eks"
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-module "helm" {
-  source                    = "./modules/helm"
-  enable_kube_state_metrics = true
-  enable_node_exporter      = true
-  providers = {
-    helm       = helm.eks
-    kubernetes = kubernetes.eks
-  }
-}
+# module "helm" {
+#   source                    = "./modules/helm"
+#   enable_kube_state_metrics = true
+#   enable_node_exporter      = true
+#   providers = {
+#     helm       = helm.eks
+#     kubernetes = kubernetes.eks
+#   }
+# }
 
 module "vpc_endpoint" {
   source = "./modules/vpc-endpoint"
--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,6 +10,8 @@ resource "aws_eks_cluster" "this" {
 
   version = var.cluster_version
 
+  bootstrap_self_managed_addons = false
+
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
     bootstrap_cluster_creator_admin_permissions = true
@@ -40,28 +42,6 @@ resource "aws_eks_cluster" "this" {
   tags = var.tags
 }
 
-# Managed add-ons
-resource "aws_eks_addon" "coredns" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "coredns"
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "kube-proxy"
-}
-
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "vpc-cni"
-}
-
-# EKS에서 관리형으로 제공되는 metrics-server
-resource "aws_eks_addon" "metrics_server" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "metrics-server"
-}
-
 resource "aws_eks_pod_identity_association" "cni" {
   count           = var.enable_irsa_cni ? 1 : 0
   cluster_name    = aws_eks_cluster.this.name
@@ -70,7 +50,7 @@ resource "aws_eks_pod_identity_association" "cni" {
   role_arn        = aws_iam_role.eks_cni_irsa_role[0].arn
 
   depends_on = [
-    aws_eks_addon.vpc_cni
+    aws_eks_cluster.this
   ]
 }
 
