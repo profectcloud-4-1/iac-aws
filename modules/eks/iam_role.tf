c0ae45c031ac4bb4203b07efa0dcac2d200d87ec
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-12-03 10:19:08 +0900

    fix: vpc cni role 중복제거

--- modules/eks/iam_role.tf
+++ modules/eks/iam_role.tf
@@ -74,27 +74,3 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSNetworkingPolicy" {
   role       = aws_iam_role.cluster.name
 }
 
-
-# ---------------------------------------
-# EKS IAM VPC CNI Role
-# ---------------------------------------
-resource "aws_iam_role" "vpc_cni" {
-  name = "eks-vpc-cni-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_cni_policy" {
-  role       = aws_iam_role.vpc_cni.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
