07539d0addb3590e8b4785233516a8c8e27f4af5
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-12-06 22:30:33 +0900

    feat: iam role for grafana

--- modules/telemetry-s3/iam_role.tf
+++ modules/telemetry-s3/iam_role.tf
@@ -77,3 +77,36 @@ resource "aws_iam_role_policy_attachment" "metric_s3" {
   role       = aws_iam_role.metric_s3.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
+
+# ---------------------------------------
+# IRSA Role for Grafana - CloudWatch access
+# ---------------------------------------
+resource "aws_iam_role" "grafana_cloudwatch" {
+  name = "eks-irsa-grafana-cloudwatch"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = var.oidc_provider_arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(var.oidc_issuer_url, "https://", "")}:aud" = "sts.amazonaws.com",
+          "${replace(var.oidc_issuer_url, "https://", "")}:sub" = "system:serviceaccount:observability:goormdotcom-grafana"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_cloudwatch_readonly" {
+  role       = aws_iam_role.grafana_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "grafana_cloudwatchlogs_readonly" {
+  role       = aws_iam_role.grafana_cloudwatch.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsReadOnlyAccess"
+}
