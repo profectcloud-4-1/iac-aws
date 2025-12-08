4d38cb14298ab22a6a6c2fc309afe06f670e6774
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-12-08 12:32:04 +0900

    fix: certmanager nodepool 지정

--- modules/k8s-certmanager/main.tf
+++ modules/k8s-certmanager/main.tf
@@ -38,7 +38,7 @@ resource "helm_release" "cert_manager" {
           "karpenter.sh/nodepool" = "system"
         }
       }
-      cert-manager = {
+      "cert-manager" = {
         nodeSelector = {
           "karpenter.sh/nodepool" = "system"
         }
