0340423bac16ec75c8ab99b6aff2edf35b5c120f
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-12-08 12:34:11 +0900

    fix: certmanager 제거

--- modules/k8s-certmanager/main.tf
+++ modules/k8s-certmanager/main.tf
@@ -15,36 +15,36 @@ terraform {
 # ---------------------------------------
 # Cert Manager
 # ---------------------------------------
-resource "helm_release" "cert_manager" {
-  name              = "cert-manager"
-  repository        = "https://charts.jetstack.io"
-  chart             = "cert-manager"
-  namespace         = "cert-manager"
-  create_namespace  = true
-  dependency_update = true
-  wait              = true
-  timeout           = 180
+# resource "helm_release" "cert_manager" {
+#   name              = "cert-manager"
+#   repository        = "https://charts.jetstack.io"
+#   chart             = "cert-manager"
+#   namespace         = "cert-manager"
+#   create_namespace  = true
+#   dependency_update = true
+#   wait              = true
+#   timeout           = 180
 
-  values = [
-    yamlencode({
-      installCRDs = true
-      cainjector = {
-        nodeSelector = {
-          "karpenter.sh/nodepool" = "system"
-        }
-      }
-      webhook = {
-        nodeSelector = {
-          "karpenter.sh/nodepool" = "system"
-        }
-      }
-      "cert-manager" = {
-        nodeSelector = {
-          "karpenter.sh/nodepool" = "system"
-        }
-      }
-    })
-  ]
-}
+#   values = [
+#     yamlencode({
+#       installCRDs = true
+#       cainjector = {
+#         nodeSelector = {
+#           "karpenter.sh/nodepool" = "system"
+#         }
+#       }
+#       webhook = {
+#         nodeSelector = {
+#           "karpenter.sh/nodepool" = "system"
+#         }
+#       }
+#       "cert-manager" = {
+#         nodeSelector = {
+#           "karpenter.sh/nodepool" = "system"
+#         }
+#       }
+#     })
+#   ]
+# }
 
 
